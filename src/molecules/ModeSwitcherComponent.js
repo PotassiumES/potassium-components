64131971577cbf27fea88b545ea116cc65190754
Trevor F. Smith <trevor@trevor.smith.name>
2018-09-15 20:33:58 -0700

    Change the mode change Component to show a temp modal with direct user input events so that it can start a new VR or XR session.

--- src/molecules/ModeSwitcherComponent.js
+++ src/molecules/ModeSwitcherComponent.js
@@ -2,6 +2,7 @@ import el from 'potassium-es/src/El'
 import App from 'potassium-es/src/App'
 import graph from 'potassium-es/src/Graph'
 import Component from 'potassium-es/src/Component'
+import DisplayModeTracker from 'potassium-es/src/DisplayModeTracker'
 
 import ButtonComponent from '../atoms/ButtonComponent.js'
 
@@ -14,7 +15,12 @@ const ModeSwitcherComponent = class extends ButtonGroupComponent {
 	constructor(dataObject=null, options={}){
 		super(dataObject, options)
 		this.addClass('mode-switcher-component')
-		this._mode = null
+		this._handleDisplayUpdate = this._handleDisplayUpdate.bind(this)
+
+		this._displayModeTracker = DisplayModeTracker.Singleton
+		this._displayModeTracker.addListener(this._handleDisplayUpdate, DisplayModeTracker.DisplayUpdatedEvent)
+
+		this._attemptingMode = null
 
 		// Exit button appears in portalEl and immersiveGraph
 		this._exitButton = new ButtonComponent(null, { text: 'Exit' })
@@ -33,7 +39,8 @@ const ModeSwitcherComponent = class extends ButtonGroupComponent {
 		this._portalButton.immersiveGraph.visible = false
 		this._portalButton.addListener((eventName, value) => {
 			if(!value)return
-			this.trigger(ModeSwitcherComponent.ModeChangedEvent, App.PORTAL)
+			this._attemptingMode = App.PORTAL
+			this._showModal()
 		}, ButtonComponent.ChangedEvent)
 		this.appendComponent(this._portalButton)
 
@@ -44,9 +51,42 @@ const ModeSwitcherComponent = class extends ButtonGroupComponent {
 		this._immersiveButton.immersiveGraph.visible = false
 		this._immersiveButton.addListener((eventName, value) => {
 			if(!value)return
-			this.trigger(ModeSwitcherComponent.ModeChangedEvent, App.IMMERSIVE)
+			this._attemptingMode = App.IMMERSIVE
+			this._showModal()
 		}, ButtonComponent.ChangedEvent)
 		this.appendComponent(this._immersiveButton)
+
+		this._switchModal = el.div(
+			{ class: 'switch-modal modal'},
+		)
+		el.button('go').appendTo(this._switchModal).addEventListener('click', ev => {
+			this._hideModal()
+			this.trigger(ModeSwitcherComponent.ModeChangedEvent, this._attemptingMode)
+			this._attemptingMode = null
+		})
+		el.button('cancel').appendTo(this._switchModal).addEventListener('click', ev => {
+			this._hideModal()
+			this._attemptingMode = null
+		})
+
+		this._updateDisplayedModes(this._displayModeTracker.portalCapable === true, this._displayModeTracker.immersiveCapable === true)
+	}
+
+	_showModal(){
+		document.body.appendChild(this._switchModal)
+	}
+
+	_hideModal(){
+		document.body.removeChild(this._switchModal)
+	}
+
+	_handleDisplayUpdate(eventName, flatCapable, portalCapable, immersiveCapable){
+		this._updateDisplayedModes(portalCapable, immersiveCapable)
+	}
+
+	_updateDisplayedModes(portalCapable, immersiveCapable){
+		this._portalButton.flatEl.style.display = portalCapable ? '' : 'none'
+		this._immersiveButton.flatEl.style.display = immersiveCapable ? '' : 'none'
 	}
 
 	handleSwitchFailed(mode){
