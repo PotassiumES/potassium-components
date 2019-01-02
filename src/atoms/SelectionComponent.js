1c8a2fc98d8b732db469201784e0f9a6b5134c7d
Trevor F. Smith <trevor@trevor.smith.name>
2019-01-02 08:00:31 -0800

    Added spatial UI to the SelectionComponent. Changed the ToggleComponent styles so that only the geometry is rotated instead of the entire component.

--- src/atoms/SelectionComponent.js
+++ src/atoms/SelectionComponent.js
@@ -3,6 +3,9 @@ import som from 'potassium-es/src/SOM'
 
 import Component from 'potassium-es/src/Component'
 
+import LabelComponent from 'potassium-components/src/atoms/LabelComponent'
+import ToggleComponent from 'potassium-components/src/atoms/ToggleComponent'
+
 /**
 SelectionComponent offers the user a set of choices for single or multiple selection.
 */
@@ -33,11 +36,94 @@ const SelectionComponent = class extends Component {
 			return item
 		})
 
-		this.options.items.forEach(item => {
+		if (this.usesSOM) {
+			const labelComponent = new LabelComponent(
+				undefined,
+				{
+					usesFlat: false,
+					usesPortalOverlay: false
+				},
+				this.inheritedOptions
+			)
+			const toggleComponent = new ToggleComponent(
+				undefined,
+				{
+					usesFlat: false,
+					usesPortalOverlay: false
+				},
+				this.inheritedOptions
+			)
+			this._spatialDisplay = new Component(
+				undefined,
+				{
+					usesFlat: false,
+					usesPortalOverlay: false,
+					labelComponent: labelComponent,
+					toggleComponent: toggleComponent
+				},
+				this.inheritedOptions
+			).appendTo(this)
+			this._spatialDisplay.addClass('selection-display')
+			this._spatialDisplay.setName('SelectionDisplay')
+			this._spatialDisplay.appendComponent(labelComponent)
+			this._spatialDisplay.appendComponent(toggleComponent)
+
+			this.listenTo(
+				Component.ActionEvent,
+				this._spatialDisplay,
+				(eventName, actionName, active, value, actionParameters) => {
+					if (actionName === '/action/activate' && active) {
+						this._spatialDisplay.hide()
+						this._spatialList.show()
+					}
+				}
+			)
+
+			this._spatialList = new Component(
+				undefined,
+				{
+					usesFlat: false,
+					usesPortalOverlay: false
+				},
+				this.inheritedOptions
+			)
+				.appendTo(this)
+				.hide()
+			this._spatialList.addClass('selection-list')
+			this._spatialList.setName('SelectionList')
+		}
+
+		this.options.items.forEach((item, index) => {
 			this.flatDOM.appendChild(dom.option({ value: item[1] }, item[0]))
 			this.portalDOM.appendChild(dom.option({ value: item[1] }, item[0]))
-			/** @todo add portal and immersive spatial controls */
+
+			if (this.usesSOM) {
+				const itemComponent = new SelectionItemComponent(
+					undefined,
+					{
+						index: index,
+						text: item[0],
+						value: item[1]
+					},
+					this._spatialList.inheritedOptions
+				).appendTo(this._spatialList)
+				this.listenTo(
+					Component.ActionEvent,
+					itemComponent,
+					(eventName, actionName, active, value, actionParameters) => {
+						if (actionName === '/action/activate' && active) {
+							this.selectedIndex = itemComponent.options.index
+							this._spatialDisplay.show()
+							this._spatialList.hide()
+						}
+					}
+				)
+			}
 		})
+
+		if (this.usesSOM) {
+			this._updateSpatialSelection()
+		}
 
 		this.listenTo('input', this.flatDOM, ev => {
 			this.selectedIndex = this.flatDOM.selectedIndex
@@ -65,10 +151,24 @@ const SelectionComponent = class extends Component {
 			changed = true
 			this.portalDOM.selectedIndex = index
 		}
-		/** @todo update the SOMs */
+
 		if (changed) {
+			this._updateSpatialSelection()
 			this.trigger(SelectionComponent.SELECTION_INDEX_CHANGED, this.flatDOM.selectedIndex)
 		}
+	}
+
+	_updateSpatialSelection() {
+		if (this.usesSOM === false) return
+		const option = this.options.items[this.selectedIndex]
+		this._spatialDisplay.options.labelComponent.text = option[0]
+	}
+}
+
+class SelectionItemComponent extends LabelComponent {
+	constructor(dataObject = null, options = {}, inheritedOptions = {}) {
+		super(dataObject, options, inheritedOptions)
+		this.addClass('selection-item')
 	}
 }
 
